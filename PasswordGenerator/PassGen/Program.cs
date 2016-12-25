aa983fae98996d2da6fc6ac48666931911c54bba
Aram <aram532@yandex.ru>
2016-12-25 18:48:30 +0400

    coder created

---
+++ PasswordGenerator/PassGen/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AghasPasswordGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var coder = new Coder();
+            Console.WriteLine(coder.Code);
+
+            Console.ReadKey();
+        }
+    }
+}
