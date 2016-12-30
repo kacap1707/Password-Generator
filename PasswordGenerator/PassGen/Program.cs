0517024415b1626a704ae485ee61781d8d98a4a1
Aram <aram532@yandex.ru>
2016-12-30 19:49:05 +0400

    at least added

--- PasswordGenerator/PassGen/Program.cs
+++ PasswordGenerator/PassGen/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PasswordGenerator;
 
 namespace AghasPasswordGenerator
 {
@@ -12,6 +13,26 @@ namespace AghasPasswordGenerator
         {
             var coder = new Coder(80);
             Console.WriteLine(coder.Code);
+
+            var pass = new Password();
+            Console.WriteLine(pass.ToString());
+            Console.WriteLine(pass.ToString().Length);
+
+            var p1 = new Password(PasswordOptions.OnlyLowerCase, 16);
+            Console.WriteLine(p1.ToString());
+            Console.WriteLine(p1.ToString().Length);
+
+            var p2 = new Password(25);
+            Console.WriteLine(p2.ToString());
+            Console.WriteLine(p2.ToString().Length);
+
+            var p3 = new Password("askd;fasfshdfksj 'l323;kj;adsfjk;");
+            Console.WriteLine(p3.ToString());
+            Console.WriteLine(p3.ToString().Length);
+
+            var p4 = new Password(PasswordOptions.OnlyAlphabet);
+            Console.WriteLine(p4.ToString());
+            Console.WriteLine(p4.ToString().Length);
 
             Console.ReadKey();
         }
--- PasswordGenerator/PasswordGenerator/Password.cs
+++ PasswordGenerator/PasswordGenerator/Password.cs
@@ -12,47 +12,68 @@ namespace PasswordGenerator
         HasUpperCase,
         OnlyUpperCase,
         OnlyLowerCase,
+        OnlyAlphabet,
         AtLeastOneDigitOneSpecialCharacter
     }
     public class Password
     {
         private readonly string password;
 
-        private const string UpperCaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        private const string LowerCaseLetters = "abcdefghijklmnopqrstuvwxyz";
         private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private const string AlphaNumericLower = "0123456789abcdefghijklmnopqrstuvwxyz";
         private const string AlphaNumericWithUpperCase = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
         private const string AlphaNumericUpper = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
+        private const string SpecialCharacters = " !#$%&'()*+,-./:;<=>?@[\"]^_`\\{|}~";
+        private const string Numbers = "0123456789";
+        #region Constructors
+        public Password()
+        {
+            password = NewPassword(PasswordOptions.AtLeastOneDigitOneSpecialCharacter).ToString();
+        }
+        public Password(int length)
+        {
+            password = NewPassword(PasswordOptions.AtLeastOneDigitOneSpecialCharacter, length).ToString();
+        }
         public Password(string str)
         {
             password = str;
         }
         public Password(PasswordOptions passOpt)
         {
-            
+            password = NewPassword(passOpt).ToString();
         }
+        public Password(PasswordOptions passOpt, int length)
+        {
+            password = NewPassword(passOpt, length).ToString();
+        }
+#endregion
         public static Password NewPassword(PasswordOptions option, int length = 32)
         {
             using (var rng = new RNGCryptoServiceProvider())
             {
                 var data = new byte[4];
-
+                var d2 = new byte[4];
                 rng.GetBytes(data);
-
+                rng.GetBytes(d2);
                 var seed = BitConverter.ToInt32(data, 0);
-
+                var seed2 = BitConverter.ToInt32(d2, 0);
                 var rnd = new Random(seed);
-
+                var r2 = new Random(seed2);
                 var passChars = new char[length];
 
                 var sb = new StringBuilder();
                 for (var i = 0; i < passChars.Length; i++)
                 {
-                    if (option == PasswordOptions.HasUpperCase) sb.Append(AlphaNumericWithUpperCase[rnd.Next(0, 63)]);
-                    if (option == PasswordOptions.OnlyUpperCase) sb.Append(AlphaNumericUpper[rnd.Next(0, 37)]);
-                    if (option == PasswordOptions.OnlyLowerCase) sb.Append(AlphaNumericLower[rnd.Next(0, 37)]);
+                    if (option == PasswordOptions.HasUpperCase) sb.Append(AlphaNumericWithUpperCase[rnd.Next(0, 62)]);
+                    if (option == PasswordOptions.OnlyUpperCase) sb.Append(AlphaNumericUpper[rnd.Next(0, 36)]);
+                    if (option == PasswordOptions.OnlyLowerCase) sb.Append(AlphaNumericLower[rnd.Next(0, 36)]);
+                    if (option == PasswordOptions.OnlyAlphabet) sb.Append(Alphabet[rnd.Next(0, 52)]);
+                    if (option == PasswordOptions.AtLeastOneDigitOneSpecialCharacter)
+                    {
+                        if (sb.Length == rnd.Next(sb.Length, length+1)) sb.Append(SpecialCharacters[rnd.Next(0, 33)]);
+                        else if (sb.Length == r2.Next(sb.Length, length+1)) sb.Append(Numbers[rnd.Next(0, 10)]);
+                        else sb.Append(Alphabet[rnd.Next(0,52)]);
+                    }
                 }
 
                 var pass = new Password(sb.ToString());
